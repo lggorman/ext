81032d567eae19e8c25843da02562a72137ea107
s-lggorman <s-lggorman@SPDR-025F3D.local>
2014-11-17 12:21:05 -0500

    added page load options

--- background.js
+++ background.js
@@ -3,9 +3,26 @@ chrome.tabs.onCreated.addListener(function(tab) {
         console.log('new tab');
         chrome.tabs.executeScript(tab.id, {file: 'jquery.js'});
         chrome.tabs.executeScript(tab.id, {file: 'custom.js'});
+
+});
         
 		
-}); 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	//doesn't work.  maybe need to add settimeout? I think response is getting sent before storage is synced
+    if (request.method == "getStorage"){
+		  chrome.storage.sync.get({
+		      'load' : 'onTab'
+		    }, function(items) {
+		        console.log(items.load); 
+		        sendResponse({data: 'lets try this'}); 
+		   });
+
+		  //sendResponse({data: 'will this work?'});
+    }
+    else{
+      sendResponse({data: 'nope'}); 
+    }
+});
 
 
 
